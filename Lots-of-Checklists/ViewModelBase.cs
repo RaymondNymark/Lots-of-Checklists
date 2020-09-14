76971b9dfb82c95ff7909403cc544d0a34f8dd81
Raymond Nymark <67239758+RaymondNymark@users.noreply.github.com>
2020-09-14 08:33:49 +0200

    Implement ViewModelBase class to raise INotifyPropertyChanged on property change

--- Lots-of-Checklists/ViewModelBase.cs
+++ Lots-of-Checklists/ViewModelBase.cs
@@ -1,12 +1,26 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Collections.Generic;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
 
 namespace Lots_of_Checklists
 {
-    class ViewModelBase
+    /// <summary>
+    /// Base class all ViewModels inherit from to automatically raise property
+    /// change every time a value is changed.
+    /// </summary>
+    public abstract class ViewModelBase : INotifyPropertyChanged
     {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected bool SetProperty<T>(ref T field, T newValue, [CallerMemberName] string propertyName = null)
+        {
+            if (!EqualityComparer<T>.Default.Equals(field, newValue))
+            {
+                field = newValue;
+                PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+                return true;
+            }
+            return false;
+        }
     }
 }
