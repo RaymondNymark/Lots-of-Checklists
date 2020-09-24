4aaa9745355fb467df2ab2b2b92a22cc9e21977c
Raymond Nymark <67239758+RaymondNymark@users.noreply.github.com>
2020-09-24 07:00:18 +0200

    Implement cleaner folder structure and create Inside & Outside Views and ViewModels

---
+++ Lots-of-Checklists/Views/OutsideView.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Lots_of_Checklists.Views
+{
+    /// <summary>
+    /// Interaction logic for OutsideView.xaml
+    /// </summary>
+    public partial class OutsideView : UserControl
+    {
+        public OutsideView()
+        {
+            InitializeComponent();
+        }
+    }
+}
