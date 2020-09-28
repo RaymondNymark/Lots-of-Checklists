aec890d295d2ea84268a70881f2694ff8a60afb6
Raymond Nymark <67239758+RaymondNymark@users.noreply.github.com>
2020-09-28 07:43:59 +0200

    Implement a ListBox ItemSource to InsideViewModel

--- Lots-of-Checklists/ViewModels/InsideViewModel.cs
+++ Lots-of-Checklists/ViewModels/InsideViewModel.cs
@@ -10,12 +10,24 @@ namespace Lots_of_Checklists.ViewModels
 {
     public class InsideViewModel : ViewModelBase
     {
-        private ChecklistsEntities _dbContext = new ChecklistsEntities();
-        public ObservableCollection<Item> CurrentItemCollectionSource
-        {
-            get => _dbContext.Checklist.FirstOrDefault(r => r.Description == "ChecklistOne").Item;
-        }
+        //private ChecklistsEntities _dbContext = new ChecklistsEntities();
+        //public ObservableCollection<Item> CurrentItemCollectionSource
+        //{
+        //    get => _dbContext.Checklist.FirstOrDefault(r => r.Description == "ChecklistOne").Item;
+        //}
 
         //public ObservableCollection<Item> ItemCollection = new ObservableCollection<Item>(_selectedChecklist.Item);
+
+        private ObservableCollection<Item> _itemSource;
+        public ObservableCollection<Item> ItemSource
+        {
+            get => _itemSource;
+            set => SetProperty(ref _itemSource, value);
+        }
+
+        public InsideViewModel(OutsideViewModel currentViewModel)
+        {
+            ItemSource = currentViewModel.SelectedChecklist.Item;
+        }
     }
 }
