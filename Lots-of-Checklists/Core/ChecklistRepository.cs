43a194fbc2d01741c2a94d4bc7f32c112226d0f2
Raymond Nymark <67239758+RaymondNymark@users.noreply.github.com>
2020-10-07 06:08:23 +0200

    Create ChecklistRepository & Exctract interface

---
+++ Lots-of-Checklists/Core/ChecklistRepository.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Lots_of_Checklists.Models;
+
+namespace Lots_of_Checklists.Core
+{
+    public class ChecklistRepository : IChecklistRepository
+    {
+        private readonly LotsOfChecklistsContext _dbContext;
+
+        public ChecklistRepository(LotsOfChecklistsContext dbContext)
+        {
+            if (dbContext == null) throw new ArgumentNullException("dbContext");
+            _dbContext = dbContext;
+        }
+
+        public Checklist Get(int checklistID)
+        {
+            return _dbContext.Set<Checklist>().Find(checklistID);
+        }
+    }
+}
---
+++ Lots-of-Checklists/Core/IChecklistRepository.cs
@@ -0,0 +1,9 @@
+﻿using Lots_of_Checklists.Models;
+
+namespace Lots_of_Checklists.Core
+{
+    public interface IChecklistRepository
+    {
+        Checklist Get(int checklistID);
+    }
+}
